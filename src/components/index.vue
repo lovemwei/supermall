68c92ee0efd6290ed207b9dc76ee8d722bb48204
willma_mw <1285215280@qq.com>
2020-09-12 09:21:03 +0800

    初始化项目

---
+++ src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'',
+    data () {
+        return {
+        }
+    },
+
+    components: {},
+
+    methods: {}
+}
+</script>
+
+<style scoped>
+</style>
